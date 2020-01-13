1bcf4dbc3ab580e59da3d7d940df10033763f3f2
Naveen Raj Shukla <naveenrajshukla@Naveens-MacBook-Pro.local>
2020-01-13 19:38:54 +0530

    minor changes

--- src/main/java/com/example/demoEmployeeKafka/employeeservice/EmployeeServiceInterface.java
+++ src/main/java/com/example/demoEmployeeKafka/employeeservice/EmployeeServiceInterface.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public interface EmployeeServiceInterface {
 
-<<<<<<< HEAD
     public <Emmployee> List<Emmployee> loadObjectList(Class<Emmployee> type, String fileName);
-=======
+
     void fileReaders();
->>>>>>> aea5a9b74cbac29137234e049fc05672235ae62a
 
 
 }
