939cce75e073129b587c79417efa56c6ca677e4e
Yorunome <mizuhashi05@gmail.com>
2020-01-13 18:07:03 +0530

    required dependencies included

---
+++ src/main/java/com/example/demoEmployeeKafka/DemoEmployeeKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoEmployeeKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEmployeeKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEmployeeKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoEmployeeKafka/DemoEmployeeKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoEmployeeKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEmployeeKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
